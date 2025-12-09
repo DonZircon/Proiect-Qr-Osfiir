4648e333706df9f927a4c22331c2558630af3331
DonZircon <romanpatrik@gmail.com>
2025-12-09 19:13:16 +0200

    inca un p

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -17,10 +17,8 @@ function Home() {
  return (
     <div className="home-container">
       <h1 className="home-title">
+        {data ? data.title : ""}
       </h1>
-      <p className="home-description">
-        Hai vasile violeaza acest prioect cu css
-      </p>
       <p className="home-description">
         Hai vasile violeaza acest prioect cu css
       </p>
