9ff4f5dac6946f72470903fbc68ad500018a31b5
unknown <barbuantonio2407@gmail.com>
2025-12-11 10:36:27 +0200

    Update frontend

--- frontend/src/about.js
+++ frontend/src/about.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import './about.css';
 
 function About() {
-    const [info, setInfo] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
